961a48b6a1e58f0afeaf86719f69bb59fe713ef7
Ruki-wtnb <shinpeww@gmail.com>
2019-12-12 13:14:30 +0000

    コメント実装完了。レイアウト整理中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require jquery
 //= require chartkick
 //= require Chart.bundle
 //= require_tree .
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,28 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new(user_id: current_user.id, jsake_id: params[:jsake_id])
+    #コメントアイコンからjsake_idを受け取る
   end
+  
+  def create #コメントの新規作成
+    comment = Comment.new(user_id: current_user.id, jsake_id: comment_params[:jsake_id], body: comment_params[:body])
+    binding.pry
+    if comment.save
+      redirect_to comment_params[:back_url], success: "コメントを投稿しました"
+    else
+      flash.now[:danger] = "コメントを投稿できませんでした"
+      new
+      render :new
+    end
+    
+    
+    
+    
+  end
+  
+  private #入力フォームからの受取
+  def comment_params
+    params.require(:comment).permit(:jsake_id, :body, :back_url)
+  end
+  
 end
