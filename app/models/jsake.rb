814585849b01444ee72ed102540994af41f1638f
Ruki-wtnb <shinpeww@gmail.com>
2021-02-06 18:39:51 +0900

    インデントなど修正

--- app/models/jsake.rb
+++ app/models/jsake.rb
@@ -1,19 +1,16 @@
 class Jsake < ApplicationRecord
   
-  validates :meigara, presence: true
-  
+  mount_uploader :image_url, ImageUploader
+  #必須の制御
+  validates :meigara, presence: true 
   validates :seimai_buai, presence: true
   validates :locaility, presence: true
   validates :alcohol_degree, presence: true
   
-  belongs_to :user
-  
+  belongs_to :user 
   has_one_attached :image
-  
-  
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: 'user'
-  
   has_many :comments
   
 end
