f3fb517a42e189445bc94b22e1b27ccd357720bf
Ruki-wtnb <shinpeww@gmail.com>
2021-08-31 14:42:11 +0900

    コメント一覧修正対応

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,10 @@ class CommentsController < ApplicationController
  def new
   @jsake = Jsake.find(params[:jsake_id])
   @comment = Comment.new(user_id: current_user.id, jsake_id: params[:jsake_id])
-  @taste = Taste.all
  end
   
  def create
   comment = Comment.new(user_id: current_user.id, jsake_id: comment_params[:jsake_id], body: comment_params[:body])
-  
   if comment.save
    redirect_to params[:comment][:back_url], success: "コメントを投稿しました"
   else
@@ -19,19 +17,18 @@ class CommentsController < ApplicationController
  end
 
  def edit
-  @jsake = Jsake.find(params[:id])
+  @jsake = Jsake.find(params[:jsake_id])
   @comment = Comment.find(params[:id])
  end
 
  def update
-  comment = Comment.find(params[:id])
+  #comment = Comment.find(params[:id])
   comment.update!(body: comment_params[:body])
   redirect_to params[:comment][:back_url], success: "コメントを修正しました" 
  end
 
  def show
   @jsake = Jsake.find(params[:id])
-  @comment = Comment.find_by(params[:id])
   @body = Comment.where(jsake_id: params[:id])
   # search_sake = []
   # search_sake.push([@jsake.meigara, @jsake.sake_meter_value.to_f, @jsake.acidity.to_f])
